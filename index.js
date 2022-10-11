c31d5a78e1d242e860df3c97c86e58b4081fa639
Eduer Pallares <eduer.pallares@zalvadora.com>
2022-10-11 01:58:34 -0500

    env

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
 const app = express();
+require('dotenv').config();
 
 app.get('/', (req, res) => {
   res.send('Hola mundo');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto: 3000');
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto: ${PORT}`);
 });
